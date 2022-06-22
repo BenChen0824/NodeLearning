845bf6172cbdb45cad74c42a9c4652a1c8099ce1
BenChen0824 <ben216033@gmail.com>
2022-06-22 10:40:03 +0800

    first node

---
+++ src/func02.js
@@ -0,0 +1,6 @@
+const f1 = (a) => a * a;
+const f2 = (a) => a * a*a;
+
+console.log('aabbcc');
+
+module.exports = {f1,f2};
---
+++ src/test_func02.js
@@ -0,0 +1,6 @@
+const {f1,f2} = require('./func02');
+const {f1:a1,f2:a2} = require('./func02');
+
+console.log(f1(5));
+console.log(a2(5));
+console.log(a1===f1);
