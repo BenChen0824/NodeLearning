7f2aa1463e4229c672f66b297e622c7c0d27f4fc
BenChen0824 <ben216033@gmail.com>
2022-06-23 11:24:42 +0800

    argv

---
+++ src/argv.js
@@ -0,0 +1,13 @@
+console.log(process.argv);
+
+/*
+在後面是傳參數用 可以直接將後面參數加到該之檔案之中
+C:\Users\Student\Desktop\node>node src\argv.js aa bb -c
+[
+  'C:\\Program Files\\nodejs\\node.exe',
+  'C:\\Users\\Student\\Desktop\\node\\src\\argv.js',
+  'aa',
+  'bb',
+  '-c'
+]
+*/
