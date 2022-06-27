01703b9d14fde0d41439c753205fe7ce50ae2a22
BenChen0824 <ben216033@gmail.com>
2022-06-27 13:10:06 +0800

    upload_images

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const multer = require("multer");
-const upload = multer({ dest: "tmp_upload/" });
+// const upload = multer({ dest: "tmp_upload/" });
+const upload = require(__dirname + "/modules/upload_imgs");
 
 app.set("view engine", "ejs");
 
@@ -29,6 +30,23 @@ app.route("/try-post-form")
 app.post("/try-upload", upload.single("avatar"), (req, res) => {
     res.json(req.file);
 });
+
+app.post("/try-uploads", upload.array("photos"), (req, res) => {
+    res.json(req.files);
+});
+
+app.get('/try-params1/:action/:id', (req, res)=>{
+    res.json({code:2, params: req.params});
+    // 這邊的要求是最嚴謹的 要放最上面
+})
+app.get('/try-params1/:action', (req, res)=>{
+    res.json({code:3, params: req.params});
+})
+app.get('/try-params1/:action?/:id?', (req, res)=>{
+    res.json({code:1, params: req.params});
+    // 這邊的要求是最鬆的 有沒有放參數都能進來 放最後
+});
+
 
 app.get("/try-qs", (req, res) => {
     res.json(req.query);
